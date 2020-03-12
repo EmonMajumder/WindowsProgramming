f1661bd49708614c711c6708b2802ea51db413fa
Emon Majumder <W0411567@campus.nscc.ca>
2020-03-12 00:43:30 -0300

    Done.

---
+++ Exercise-5/Exercise-5/Data.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Data
+    {
+        private String firstname;
+        private String lastname;
+        private String birthdate;
+        private String email;
+
+        public String FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public String LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        public String BirthDate
+        {
+            get { return birthdate; }
+            set { birthdate = value; }
+        }
+
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
